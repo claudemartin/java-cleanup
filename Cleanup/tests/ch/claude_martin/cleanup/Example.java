900aa6b989519a25a4a7d3bfd31a1de2c20a3aa4
trucedanimal@gmail.com <trucedanimal@gmail.com@318322e0-8592-f964-8dd9-aea0d773e380>
2014-05-29 17:00:53 +0000

    Example improved.

--- Cleanup/tests/ch/claude_martin/cleanup/Example.java
+++ Cleanup/tests/ch/claude_martin/cleanup/Example.java
@@ -1,11 +1,18 @@
 package ch.claude_martin.cleanup;
 
+import static java.util.Collections.newSetFromMap;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 /**
@@ -50,36 +57,50 @@ public final class Example implements Cleanup {
     // We DO NOT use a finalizer!!
   };
 
+  /**
+   * Run this in a console (without arguments).
+   * It will tell you how long an object existed until cleanup was invoked.
+   */
   public static void main(String[] args) throws Exception {
     // We can register global exception handlers:
     Cleanup.addExceptionHandler((ex) -> {
       logger.warning(ex.toString());
     });
-    // A boolean that will be set to 'true' on cleanup:
-    final AtomicBoolean b = new AtomicBoolean(false);
+    // A boolean that will be set to someting larget than 0 on cleanup:
+    final AtomicLong ms = new AtomicLong(0L);
     // An instance of the 'Example':
     Example example = new Example();
-    example.registerCleanup((v) -> {
-      // This is just to test and demonstrate the functionality:
-        v.set(true);
-      }, b);
+    example.registerCleanup((then) -> {
+      // How long until this cleanup is done?
+        final long nanos = System.nanoTime() - then;
+        ms.set(nanos / 1_000_000L); // = milliseconds
+      }, System.nanoTime()); // measured right after 'example' was created.
+    
+    // The cleanup code was not executed yet!
+    if (ms.get() != 0)
+      throw new RuntimeException("Something went wrong.");
+    
+    System.out.println("The object is created.");
+    System.out.println("Hit <ENTER> to have it removed.");
+    System.in.read();
+    
     // We want it to be removed and cleaned up:
     example = null; // free to be removed by GC.
-
     // This is just for the test, as the cleanup-thread also has minimum priority:
     Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
     // Now we try to really get rid of it:
-    for (int i = 0; i < 10; i++) {
+    for (int n = 0; n < 10; n++) {
       // Allow GC and CleanupDaemon to work:
       Thread.sleep(10);
       System.gc();
     }
 
     // Let's see if it worked:
-    if (!b.get()) {
+    if (ms.get() <= 0) {
       System.err.println("Sadly this didn't work :-(");
     } else {
-      System.out.println("Hooray, the resource was closed :-)");
+      System.out.format("Hooray, the resource was closed after about %,d milliseconds :-)",
+          ms.get());
     }
   }
 }
