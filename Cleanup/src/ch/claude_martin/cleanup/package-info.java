30e0a964fad8381416523d48b553bd51ba4d3fe0
claudemartin <claude.martin@students.fhnw.ch>
2015-03-19 09:44:12 +0100

    javadoc.

--- Cleanup/src/ch/claude_martin/cleanup/package-info.java
+++ Cleanup/src/ch/claude_martin/cleanup/package-info.java
@@ -1,27 +1,29 @@
 /**
  * This package contains the interface {@link ch.claude_martin.cleanup.Cleanup}, which makes it 
- * easy to register an action to clean up after some object has been removes by garbage collection.
+ * easy to register an action to clean up after some object has been removed by garbage collection.
  * It solves some problems that are common with finalizer blocks, but it has it's own pitfalls.
  * <p>Note that this can only be used with Java 8 or newer. Older versions of Java do not allow 
  * lambdas and there use of a "Finalizer Guardian" is probably more convenient.
  * 
  * <p>
  * Projekt Home: 
- * <a href="https://code.google.com/p/java-cleanup/">https://code.google.com/p/java-cleanup/</a>
- * |
- * <a href="http://claude-martin.ch/java-cleanup/">http://claude-martin.ch/java-cleanup/</a>
+ * <a href="https://github.com/claudemartin/java-cleanup">github.com/claudemartin/java-cleanup</a> | <a 
+ * href="http://claude-martin.ch/java-cleanup/">claude-martin.ch/java-cleanup/</a>
  * 
- * <p>I provide this code as is, with no guarantees. The MIT license follows: 
- * 
- * <p>
- * The MIT License (MIT)
+ * <p>I, Claude Martin, provide this code as is, with no guarantees. The MIT license follows: 
  * 
  * <p>
- * Copyright (c) 2014 Claude Martin Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <strong>The MIT License (MIT)</strong>
+ * 
+ * <p>
+ * Copyright (c) 2014 Claude Martin 
+ * 
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
  * 
  * <p>
  * The above copyright notice and this permission notice shall be included in all copies or
